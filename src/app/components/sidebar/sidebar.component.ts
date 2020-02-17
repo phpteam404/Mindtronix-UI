4f6573468dbf3c764890713d2a93583082a512f9
phpteam404 <php@thresholdsoft.com>
2020-02-17 11:24:44 +0530

    sidebar custo sroll and child menu started

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -23,26 +23,44 @@ export const ROUTES: RouteInfo[] =[];/* [
 })
 export class SidebarComponent implements OnInit {
   menuItems: any[];
+  breadcrumbList: Array<any> = [];
+  name: string;
+  menu:any=[];
 
   constructor(private ls: LocalStorageService,private router: Router) { }
-
   ngOnInit() {
-    
-    var menu = JSON.parse(this.ls.getItem('user')).menu;
-    if(menu){
+    this.menu = JSON.parse(this.ls.getItem('user')).menu;
+    if(this.menu){
       this.menuItems=[];
-      menu.forEach(item => { 
+      this.menu.forEach(item => { 
         var obj={
           path : item.module_url,
           title : item.module_name,
           icon : '',
           class:item.module_icon,
-          child:item.childs
+          childs:item.sub_menus
         };
         this.menuItems.push(obj);
       });
+      this.menuItems.forEach(item=> {
+        if(item.childs){
+          var childs=[];
+          childs = item.childs;
+          item.childs =[];
+          childs.forEach(ch => { 
+            var obj={
+              path : ch.module_url,
+              title : ch.module_name,
+              icon : '',
+              class:ch.module_icon
+            };
+            item.childs.push(obj);
+          });
+        }
+      });
       //this.menuItems = ROUTES.filter(menuItem => menuItem);
       console.log('menuItems--',  this.menuItems );
+      this.listenRouting();
     }else{
       this.ls.removeItem('user');
       this.router.navigate(['/login']);
@@ -55,4 +73,38 @@ export class SidebarComponent implements OnInit {
       //}
       return true;
   };
+
+
+  /* Listening for routing events */
+  listenRouting() {
+    console.log('called listenRouting---',this.router.events);
+    let routerUrl: string, routerList: Array<any>, target: any;
+    this.router.events.subscribe((router: any) => {
+      routerUrl = router.urlAfterRedirects;
+      if (routerUrl && typeof routerUrl === 'string') {
+        // initialization breadcrumb
+        target = this.menu;
+        this.breadcrumbList.length = 0;
+        // Get the current routing URL / zone, [0] = first layer, [1] = second layer ... etc
+        routerList = routerUrl.slice(1).split('/');
+        routerList.forEach((router, index) => {
+          // Find the path of this layer in the menu and the same path as the current routing
+        target = target.find(page => page.path.slice(2) === router);
+          // After saving to the breadcrumbList, the list will be looped directly after the time is up.
+        this.breadcrumbList.push({
+            name: target.name,
+            // The routing of the second layer must be added to the routing of the previous layer. Using relative positions will cause routing errors.
+            path: (index === 0) ? target.path : `${this.breadcrumbList[index-1].path}/${target.path.slice(2)}`
+          });
+          
+          // The target of the next layer to be compared is the subpage specified by this layer
+          if (index+1 !== routerList.length) {
+            target = target.children;
+          }
+        });
+
+        console.log('this.breadcrumbList===>>>',this.breadcrumbList);
+      }
+    });
+  }
 }
