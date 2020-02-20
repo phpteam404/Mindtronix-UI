64e7242dbd5ebd7368d5b4bf903fde083105da62
phpteam404 <57086837+phpteam404@users.noreply.github.com>
2020-02-20 16:46:21 +0530

    Fanchise, student, school changes

--- src/app/Modules/schools/school-list/school-list.component.ts
+++ src/app/Modules/schools/school-list/school-list.component.ts
@@ -20,26 +20,27 @@ export class SchoolListComponent implements OnInit {
         {label:'Paris', value:{id:5, name: 'Paris', code: 'PRS'}}
     ];
     this.cars = [
-      {no_of_students:'10', school_name:'About Academic Heights Public School - AHPS', address:'Plot No. 153, Air By Pass Road, Near M.R. Palli Circle', phone_no:'9000867321', email:'academicheights@gmail.com',last_update:'01-02-2020'},
-      {no_of_students:'10', school_name:'Accord School', address:'RC Road, Chiguruwada ', phone_no:'9908729577', email:'accordschool@gmail.com',last_update:'07-02-2020'},
-      {no_of_students:'10', school_name:'Adarsha Vidyalaya High School', address:'G.N. Mada Street', phone_no:'9393607734', email:'Pending',last_update:'02-02-2020'},
-      {no_of_students:'10', school_name:'Akshara High School', address:'Nawab Pet, Near Head Post Office', phone_no:'(0877) 2256375', email:'aksharaschool@gmail.com',last_update:'04-02-2020'},
-      {no_of_students:'10', school_name:'Balaji English School', address:'Madhura Nagar, Railway Colony', phone_no:'(0877) 2226122', email:'balajischool@gmail.com',last_update:'05-02-2020'},
-      {no_of_students:'10', school_name:'BCM High School', address:'5/677, Mangalam Quarters', phone_no:'9000371258', email:'bcmhighschool@gmail.com',last_update:'06-02-2020'},
-      {no_of_students:'10', school_name:'Bharathi Vidyanikethan', address:'935/B, Korlagunta', phone_no:'(0877) 6571287', email:'bharathischool@gmail.com',last_update:'08-02-2020'},
-      {no_of_students:'10', school_name:'Bharatiya Vidya Bhavan', address:'Post Box No. 12, Bhavan\'s Campus Alipiri', phone_no:'(0877) 2288657', email:'bvidyabhavan@gmail.com',last_update:'09-02-2020'},
-      {no_of_students:'10', school_name:'Bhashyam Public School', address:'S.D.N. Buiildings, Air Bye Pass Riad, Balaji Colony', phone_no:'(0877) 289174 ', email:'bhashyamschoold@gmail.com',last_update:'12-02-2020'},
-      {no_of_students:'10', school_name:'Bhavan\'s S.V. Vidyalaya', address:'Post Box No. 12, Bhavan\'s Campus, Alipiri', phone_no:'(0877) 2288657', email:'bhavansvschool@gmail.com',last_update:'10-02-2020'},
-      {no_of_students:'10', school_name:'Bhupal Junior College', address:'1, R.C. Road, Near Raithu Bazaar', phone_no:'9396247397', email:'bhupaljrcollege@gmail.com',last_update:'11-02-2020'},
-      {no_of_students:'10', school_name:'Birla Open Minds International School', address:'Opp. Thondavada Village, Tirupati-Bengaluru Highway', phone_no:'7036620222', email:'birlaopenmindstpty@gmail.com',last_update:'13-02-2020'},
-      {no_of_students:'10', school_name:'Brilliant High School', address:'RS Gardens', phone_no:'(0877) 2230911', email:'brillianthighschool@gmail.com',last_update:'03-02-2020'},
-      {no_of_students:'10', school_name:'CAN Junior College', address:'K.T. Road, Opposite E- Seva Kendram', phone_no:'9849817008', email:'canjrcollege@gmail.com',last_update:'14-02-2020'},
-      {no_of_students:'10', school_name:'Chaitanya Bharathi School', address:'Hatirai Colony', phone_no:'(0877) 2240455', email:'cbschool@gmail.com',last_update:'15-02-2020'},
-      {no_of_students:'10', school_name:'Dr. KKR’s Gowtham (E.M.) High School', address:'K.T. Road, Varadaraja Nagar', phone_no:'9160100222', email:'kkrschool@gmail.com',last_update:'16-02-2020'},
-      {no_of_students:'10', school_name:'Edify School', address:'Mohan Gardens, Vaishnavi Nagar, Tiruchanoor', phone_no:'9505908888', email:'edifyschool@gmail.com',last_update:'17-02-2020'},
-      {no_of_students:'10', school_name:'Narayana Concept School', address:'Near Srinivasa Kalyana Mandapam, Tiruchur Road, Srinivasa Puram', phone_no:'9912341566', email:'narayanaschool@gmail.com',last_update:'18-02-2020'}
+      {code:'SC_001', no_of_students:'10', school_name:'About Academic Heights Public School - AHPS', address:'Plot No. 153, Air By Pass Road, Near M.R. Palli Circle', phone_no:'9000867321', email:'academicheights@gmail.com',last_update:'01-02-2020'},
+      {code:'SC_002', no_of_students:'10', school_name:'Accord School', address:'RC Road, Chiguruwada ', phone_no:'9908729577', email:'accordschool@gmail.com',last_update:'07-02-2020'},
+      {code:'SC_003', no_of_students:'10', school_name:'Adarsha Vidyalaya High School', address:'G.N. Mada Street', phone_no:'9393607734', email:'Pending',last_update:'02-02-2020'},
+      {code:'SC_004', no_of_students:'10', school_name:'Akshara High School', address:'Nawab Pet, Near Head Post Office', phone_no:'(0877) 2256375', email:'aksharaschool@gmail.com',last_update:'04-02-2020'},
+      {code:'SC_005', no_of_students:'10', school_name:'Balaji English School', address:'Madhura Nagar, Railway Colony', phone_no:'(0877) 2226122', email:'balajischool@gmail.com',last_update:'05-02-2020'},
+      {code:'SC_006', no_of_students:'10', school_name:'BCM High School', address:'5/677, Mangalam Quarters', phone_no:'9000371258', email:'bcmhighschool@gmail.com',last_update:'06-02-2020'},
+      {code:'SC_007', no_of_students:'10', school_name:'Bharathi Vidyanikethan', address:'935/B, Korlagunta', phone_no:'(0877) 6571287', email:'bharathischool@gmail.com',last_update:'08-02-2020'},
+      {code:'SC_008', no_of_students:'10', school_name:'Bharatiya Vidya Bhavan', address:'Post Box No. 12, Bhavan\'s Campus Alipiri', phone_no:'(0877) 2288657', email:'bvidyabhavan@gmail.com',last_update:'09-02-2020'},
+      {code:'SC_009', no_of_students:'10', school_name:'Bhashyam Public School', address:'S.D.N. Buiildings, Air Bye Pass Riad, Balaji Colony', phone_no:'(0877) 289174 ', email:'bhashyamschoold@gmail.com',last_update:'12-02-2020'},
+      {code:'SC_010', no_of_students:'10', school_name:'Bhavan\'s S.V. Vidyalaya', address:'Post Box No. 12, Bhavan\'s Campus, Alipiri', phone_no:'(0877) 2288657', email:'bhavansvschool@gmail.com',last_update:'10-02-2020'},
+      {code:'SC_011', no_of_students:'10', school_name:'Bhupal Junior College', address:'1, R.C. Road, Near Raithu Bazaar', phone_no:'9396247397', email:'bhupaljrcollege@gmail.com',last_update:'11-02-2020'},
+      {code:'SC_012', no_of_students:'10', school_name:'Birla Open Minds International School', address:'Opp. Thondavada Village, Tirupati-Bengaluru Highway', phone_no:'7036620222', email:'birlaopenmindstpty@gmail.com',last_update:'13-02-2020'},
+      {code:'SC_013', no_of_students:'10', school_name:'Brilliant High School', address:'RS Gardens', phone_no:'(0877) 2230911', email:'brillianthighschool@gmail.com',last_update:'03-02-2020'},
+      {code:'SC_014', no_of_students:'10', school_name:'CAN Junior College', address:'K.T. Road, Opposite E- Seva Kendram', phone_no:'9849817008', email:'canjrcollege@gmail.com',last_update:'14-02-2020'},
+      {code:'SC_015', no_of_students:'10', school_name:'Chaitanya Bharathi School', address:'Hatirai Colony', phone_no:'(0877) 2240455', email:'cbschool@gmail.com',last_update:'15-02-2020'},
+      {code:'SC_016', no_of_students:'10', school_name:'Dr. KKR’s Gowtham (E.M.) High School', address:'K.T. Road, Varadaraja Nagar', phone_no:'9160100222', email:'kkrschool@gmail.com',last_update:'16-02-2020'},
+      {code:'SC_017', no_of_students:'10', school_name:'Edify School', address:'Mohan Gardens, Vaishnavi Nagar, Tiruchanoor', phone_no:'9505908888', email:'edifyschool@gmail.com',last_update:'17-02-2020'},
+      {code:'SC_018', no_of_students:'10', school_name:'Narayana Concept School', address:'Near Srinivasa Kalyana Mandapam, Tiruchur Road, Srinivasa Puram', phone_no:'9912341566', email:'narayanaschool@gmail.com',last_update:'18-02-2020'}
     ];
     this.cols = [
+      { field: 'code', header: 'School code' },
       { field: 'school_name', header: 'School Name' },
       { field: 'no_of_students', header: 'Number of Students' },
       { field: 'phone_no', header: 'Phone no' },
@@ -54,6 +55,9 @@ export class SchoolListComponent implements OnInit {
 
   AddNewSchool(event: Event){
     this.router.navigate(['add'], {relativeTo: this._route});
+  }
+  GoToStudent(event: Event){
+    this.router.navigate(['users/students'], {});
   }
 
 }
--- src/app/Modules/user/Students/student-list/student-list.component.ts
+++ src/app/Modules/user/Students/student-list/student-list.component.ts
@@ -21,30 +21,33 @@ export class StudentListComponent implements OnInit {
         {label:'Paris', value:{id:5, name: 'Paris', code: 'PRS'}}
     ];
     this.cars = [
-      {name:'Grab Kit', email:'grabKit@mindtronics.com', phone:'7894564556', manager:'Mike',actions:''},
-      {name:'Tech Point', email:'tech_point@mindtronics.com', phone:'7418523654', manager:'Peter',actions:''},
-      {name:'Mindex', email:'mindex@mindtronics.com', phone:'9632587412', manager:'Jenny',actions:''},
-      {name:'Grade Power', email:'grade_power@mindtronics.com', phone:'6987452541', manager:'Lisa',actions:''},
-      {name:'Tera Soft', email:'teraSoft@mindtronics.com', phone:'8965412547', manager:'John II',actions:''},
-      {name:'Brain Balance', email:'brain-blnc@mindtronics.com', phone:'7588965415', manager:'John',actions:''},
-      {name:'Learning Horizon', email:'lhorizon@mindtronics.com', phone:'9658745896', manager:'Peter',actions:''},
-      {name:'Lesson Up', email:'lessonUp@mindtronics.com', phone:'9658745896', manager:'Karen',actions:''},
-      {name:'Brain Gym', email:'gym-brain@mindtronics.com', phone:'8569745896', manager:'Dominique',actions:''},
-      {name:'Robo Soft', email:'robosoft@mindtronics.com', phone:'7545896552', manager:'Christian',actions:''},
-      {name:'Simply Brilliant', email:'sbrilliant@mindtronics.com', phone:'9632568745', manager:'Jimmy',actions:''},
-      {name:'Inquistive', email:'inquistive@mindtronics.com', phone:'7895874568', manager:'Trump',actions:''},
-      {name:'People Tech', email:'people_tech@mindtronics.com', phone:'9658523645', manager:'Donald',actions:''},
-      {name:'Bulb', email:'bulb@mindtronics.com', phone:'7845258963', manager:'Modi',actions:''},
-      {name:'Newton', email:'newton@mindtronics.com', phone:'8547896541', manager:'Jack',actions:''},
-      {name:'Study Point', email:'spoint@mindtronics.com', phone:'7458963521', manager:'Bill Gates',actions:''},
-      {name:'Robo Tech', email:'robo-tech@mindtronics.com', phone:'6985236545', manager:'Obama',actions:''},
-      {name:'Tutor Pedia', email:'tpedia@mindtronics.com', phone:'8569874589', manager:'Laura',actions:''}
+      {status:'Active', grade:'VII', city:'Tirupathi', name:'Grab Kit', email:'grabKit@mindtronics.com', phone:'7894564556', manager:'Mike',actions:''},
+      {status:'Active', grade:'VII', city:'Tirupathi', name:'Tech Point', email:'tech_point@mindtronics.com', phone:'7418523654', manager:'Peter',actions:''},
+      {status:'Inactive', grade:'IV', city:'Hyderabad', name:'Mindex', email:'mindex@mindtronics.com', phone:'9632587412', manager:'Jenny',actions:''},
+      {status:'Active', grade:'VIII', city:'Tirupathi', name:'Grade Power', email:'grade_power@mindtronics.com', phone:'6987452541', manager:'Lisa',actions:''},
+      {status:'Active', grade:'VII', city:'Tirupathi', name:'Tera Soft', email:'teraSoft@mindtronics.com', phone:'8965412547', manager:'John II',actions:''},
+      {status:'Inactive', grade:'IX', city:'Banglore', name:'Brain Balance', email:'brain-blnc@mindtronics.com', phone:'7588965415', manager:'John',actions:''},
+      {status:'Active', grade:'VIII', city:'Tirupathi', name:'Learning Horizon', email:'lhorizon@mindtronics.com', phone:'9658745896', manager:'Peter',actions:''},
+      {status:'Active', grade:'VII', city:'Tirupathi', name:'Lesson Up', email:'lessonUp@mindtronics.com', phone:'9658745896', manager:'Karen',actions:''},
+      {status:'Active', grade:'V', city:'Hyderabad', name:'Brain Gym', email:'gym-brain@mindtronics.com', phone:'8569745896', manager:'Dominique',actions:''},
+      {status:'Inactive', grade:'VII', city:'Tirupathi', name:'Robo Soft', email:'robosoft@mindtronics.com', phone:'7545896552', manager:'Christian',actions:''},
+      {status:'Active', grade:'VII', city:'Tirupathi', name:'Simply Brilliant', email:'sbrilliant@mindtronics.com', phone:'9632568745', manager:'Jimmy',actions:''},
+      {status:'Inactive', grade:'VIII', city:'Tirupathi', name:'Inquistive', email:'inquistive@mindtronics.com', phone:'7895874568', manager:'Trump',actions:''},
+      {status:'Active', grade:'VII', city:'Tirupathi', name:'People Tech', email:'people_tech@mindtronics.com', phone:'9658523645', manager:'Donald',actions:''},
+      {status:'Active', grade:'VII', city:'Tirupathi', name:'Bulb', email:'bulb@mindtronics.com', phone:'7845258963', manager:'Modi',actions:''},
+      {status:'Active', grade:'VII', city:'Tirupathi', name:'Newton', email:'newton@mindtronics.com', phone:'8547896541', manager:'Jack',actions:''},
+      {status:'Active', grade:'VI', city:'Tirupathi', name:'Study Point', email:'spoint@mindtronics.com', phone:'7458963521', manager:'Bill Gates',actions:''},
+      {status:'Active', grade:'VII', city:'Tirupathi', name:'Robo Tech', email:'robo-tech@mindtronics.com', phone:'6985236545', manager:'Obama',actions:''},
+      {status:'Active', grade:'VII', city:'Tirupathi', name:'Tutor Pedia', email:'tpedia@mindtronics.com', phone:'8569874589', manager:'Laura',actions:''}
     ];
     this.cols = [
-      { field: 'name', header: 'Name' },
-      { field: 'manager', header: 'Parent' },
+      { field: 'name', header: 'Student Name' },
+      { field: 'manager', header: 'Parent Name' },
+      { field: 'grade', header: 'Grade' },
+      { field: 'city', header: 'City' },
       { field: 'email', header: 'Email' },
       { field: 'phone', header: 'Phone' },
+      { field: 'status', header: 'Status' },
       { field: 'actions', header: 'Actions' }
     ];
   }
