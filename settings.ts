51247e51db892b7cb5aad28d7ef321fbb4963b0c
phpteam404 <57086837+phpteam404@users.noreply.github.com>
2020-03-30 10:08:15 +0530

    New URL Settings

--- settings.ts
+++ settings.ts
@@ -1,9 +1,10 @@
 export const settings = {
     // API_URL: 'http://192.168.0.19/Mindtronix/rest/index.php/',
-     API_URL: ' http://192.168.0.63/Mindtronixs/rest/',
-    // API_URL: 'http://139.59.59.231/Mindtronix/rest/index.php/',
+    // API_URL: ' http://192.168.0.12/Mindtronixs/rest/',
+    API_URL: 'http://139.59.59.231/Mindtronix_prototype/rest/index.php/',
+    // API_URL: 'http://139.59.59.231/Mindtronix_test/rest/index.php/',
     APP_STORAGE_KEY: 'sessionUser',
-    PRD_ASSESTS_PATH: '',// /Mindtronix_prototype
+    PRD_ASSESTS_PATH: 'Mindtronix_prototype',// /Mindtronix_prototype /Mindtronix_test
     PRODUCTION: true,
     DEFAULT_LANG:'en',
     MAX_UPLOAD:20000000, //(in bytes)
