8ffe510dc29eb8f26350f7894a3f279230804504
phpteam404 <57086837+phpteam404@users.noreply.github.com>
2020-03-30 19:02:44 +0530

    Settings commit

--- settings.ts
+++ settings.ts
@@ -1,10 +1,18 @@
 export const settings = {
-    // API_URL: 'http://192.168.0.19/Mindtronix/rest/index.php/',
-    // API_URL: ' http://192.168.0.12/Mindtronixs/rest/',
-    API_URL: 'http://139.59.59.231/Mindtronix/rest/index.php/',
+    // Local
+    PRD_ASSESTS_PATH: '',// /Mindtronix_prototype /Mindtronix_test
+    API_URL: ' http://192.168.0.12/Mindtronixs/rest/',
+
+    // Mindtronix_prototype
+    // PRD_ASSESTS_PATH: '/Mindtronix_prototype',// /Mindtronix_prototype /Mindtronix_test
+    // API_URL: 'http://139.59.59.231/Mindtronix/rest/index.php/',
+
+    //Mindtronix_test
+    // PRD_ASSESTS_PATH: '/Mindtronix_test',// /Mindtronix_prototype /Mindtronix_test
     // API_URL: 'http://139.59.59.231/Mindtronix_test/rest/index.php/',
+
+
     APP_STORAGE_KEY: 'sessionUser',
-    PRD_ASSESTS_PATH: 'Mindtronix_prototype',// /Mindtronix_prototype /Mindtronix_test
     PRODUCTION: true,
     DEFAULT_LANG:'en',
     MAX_UPLOAD:20000000, //(in bytes)
